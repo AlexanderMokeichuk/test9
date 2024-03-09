5c97b000ba60301e2427f912c733415d70bda46a
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-03-09 12:27:14 +0600

    Added components "Header.tsx", "NavBar.tsx"

---
+++ src/component/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header:React.FC = () => {
+  return (
+    <header>
+      <div className={"container"}>
+
+      </div>
+    </header>
+  );
+};
+
+export default Header;
